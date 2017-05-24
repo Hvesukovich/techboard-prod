80f9ef48efb318454eb53cf9b776c9bfd781ea38
Sergei <3Hopkins@mail.ru>
2017-05-24 15:59:54 +0300

    Сделал вывод всех категория + на каждую категорию по таблице для товаров в "user-goods"

--- angular/src/app/components/goods/goods.component.ts
+++ angular/src/app/components/goods/goods.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CategoriesService} from "../../services/categories.service";
 
 @Component({
   selector: 'app-goods',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoodsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private categoriesService: CategoriesService) { }
 
   ngOnInit() {
   }
